ce4748b227845f59b883941bc1fd9d988445c6ac
Muhwezi Armstrong <armstrongsouljah@gmail.com>
2019-03-04 09:44:43 +0300

    chore(routing): set up react router
    - install react router library
    - setup dummy routes
    - refactor entry file to render routes instead of root app component
    
    [Maintains #164352866]

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const App = () => (
     <div>
         <h1>Welcome to store manager</h1>
+        <ul>
+            <li><a href="/products">Products</a></li>
+        </ul>
+        
     </div>
 );
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,42 +1,42 @@
-var webpack = require('webpack');
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
-var parentDir = path.join(__dirname, '../src/');
+var parentDir = path.join(__dirname, "../src/");
 
 module.exports = {
     entry: [
-        path.join(parentDir, 'index.js')
+        path.join(parentDir, "index.js"),
     ],
     resolve: {
-        extensions: [".jsx", ".js"]
+        extensions: [".jsx", ".js",],
     },
-    mode: 'development',
+    mode: "development",
     module: {
         rules: [{
             test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },{
-                test: /\.less$/,
-                loaders: ["style-loader", "css-loder", "less-loader"]
-            }
-        ]
+            exclude: /node_modules/,
+            loader: "babel-loader",
+        },{
+            test: /\.less$/,
+            loaders: ["style-loader", "css-loder", "less-loader",],
+        },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-          }),
+            template: "./src/index.html",
+            filename: "./index.html",
+        }),
     ],
     output: {
-        path: parentDir + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
+        path: parentDir + "/dist",
+        publicPath: "/",
+        filename: "bundle.js",
     },
     devServer: {
         contentBase: parentDir,
-        historyApiFallback: true
-    }
-}
+        historyApiFallback: true,
+    },
+};
