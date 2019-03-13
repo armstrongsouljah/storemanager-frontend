a94793896d270559117cc8ac21161680a0ba88af
Muhwezi Armstrong <armstrongsouljah@gmail.com>
2019-03-13 12:16:21 +0300

    fix(feedback): Implement ui feedback
    - Make the table standout
    - Align the content properly
    
    [Fixes #164594803]

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,22 +5,22 @@ const renderProducts = (data) => {
     if (data.products) {
         return data.products.map(product => (
             <tr key={product.product_id}>
-                <td>{product.product_name}</td>
+                <td className="uppercase-first">{product.product_name}</td>
                 <td>{product.quantity}</td>
-                <td>{product.unit_cost}</td>
+                <td className="right-align">{product.unit_cost}</td>
             </tr>
         ));
     }
 };
 const Dashboard = props => (
-    <div className="col s12  blue lighten-5">
-        <h6 className="center">Product Listing</h6>
-        <table className="responsive-table centered">
+    <div className="col s12 white">
+        <h4 className="center">Product Listing</h4>
+        <table className="responsive-table striped">
             <thead>
-                <tr>
-                    <th>Product Name</th>
-                    <th>Quantity</th>
-                    <th>Unit Cost</th>
+                <tr className="blue darken-1 white-text">
+                    <th><h5>Product Name</h5></th>
+                    <th><h5>Quantity</h5></th>
+                    <th className="right-align"><h5>Unit Cost</h5></th>
                 </tr>
             </thead>
             <tbody>
